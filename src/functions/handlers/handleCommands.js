0eb9cdd42924b2ec544dd4fa448cc63bd02b34d6
hdpham22 <103155924+hdpham22@users.noreply.github.com>
2022-12-24 11:39:50 -0800

    Add files via upload

---
+++ src/functions/handlers/handleCommands.js
@@ -0,0 +1,33 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const fs = require('fs');
+
+module.exports = (client) => {
+    client.handleCommands = async () => {
+        const commandFolders = fs.readdirSync('./src/commands');
+        for (const folder of commandFolders) {
+            const commandFiles = fs
+            .readdirSync(`./src/commands/${folder}`)
+            .filter(file => file.endsWith('.js'));
+
+            const { commands, commandArray } = client;
+            for (const file of commandFiles) {
+                const command = require(`../../commands/${folder}/${file}`);
+                commands.set(command.data.name, command);
+                commandArray.push(command.data.toJSON());
+                console.log(`Command: ${command.data.name} has been passed`);
+            }
+        }
+        const clientId = '1055756551886995506';
+        const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+        try {
+            console.log('Started refreshing application (/) commands.');
+
+            await rest.put(Routes.applicationCommands(clientId), {
+                body: client.commandArray,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+};
---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { Client, GatewayIntentBits, Partials, Collection, Events } = require('discord.js');
+require('dotenv/config');
+const fs = require('fs')
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.commands = new Collection();
+client.commandArray = [];
+
+const functionFolders = fs.readdirSync('./src/functions');
+for (const folder of functionFolders) {
+    const functionFiles = fs
+        .readdirSync(`./src/functions/${folder}`)
+        .filter((file) => file.endsWith('.js'));
+    for (const file of functionFiles) 
+        require(`./functions/${folder}/${file}`)(client);
+}
+
+/*client.on('ready', () => {
+    console.log('FatBot is online')
+})*/
+
+client.handleEvents();
+client.handleCommands();
+
+client.login(process.env.TOKEN)
