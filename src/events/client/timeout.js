ccaa56092c39bb3504a39a6e1cd590b1ba01ae39
hdpham22 <103155924+hdpham22@users.noreply.github.com>
2022-12-28 21:51:49 -0800

    Update timeout.js

--- src/events/client/timeout.js
+++ src/events/client/timeout.js
@@ -3,9 +3,15 @@ const { channelLink } = require("discord.js");
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
-        if (message.content.includes('fat')){
-            message.channel.send('You have no friends');
+        if (message.content.toLowerCase().includes('fat')) {
+            if (message.member.manageable) {
+                message.member.timeout(60 * 1000).catch(console.error);
+                message.channel.send('You have no friends');
+            } else {
+                message.channel.send('You\'re lucky I don\'t have the permissions to put you on timeout.');
+            }
+
         }
-        
+
     }
 }
